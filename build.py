3fd440d19a522ee4b15f772b35aba8785329a825
Robert <robertchen@live.com>
2020-02-10 23:25:22 +0000

    Added util.py hash_file

--- build.py
+++ build.py
@@ -1,8 +1,29 @@
 import os, yaml
+import hash_file from util
 
 config = eval(open("config/config.json", "r").read().strip())
 xinetd_template = open("config/ctf.xinetd.sample", "r").read().strip()
 compose_template = open("config/docker-compose.yml.sample", "r").read().strip()
+
+def generate_xinted(problem, data):
+  f = open(problem + "/ctf.xinetd", "w")
+  xinted = xinetd_template.replace("[BINARY]", data["binary"])
+
+  f.write(xinted)
+  f.close()
+
+def generate_docker(problem, data):
+  if not os.path.exists(problem + "/docker-compose.yml"):
+    if os.path.exists(problem + "/Dockerfile"):
+      f = open(problem + "/docker-compose.yml", "w")
+      compose = compose_template.replace("[PORT]", str(data["port"]))
+    
+      f.write(compose)
+      f.close()
+
+      print("Successfully generated docker-compose.yml for " + problem)
+    else:
+      print("Skipping docker for " + problem)
 
 if __name__ == "__main__":
   baseDir = config["problemDirectory"]
@@ -18,23 +39,9 @@ if __name__ == "__main__":
           data = yaml.safe_load(stream)
 
           if "binary" in data:
-            f = open(problem + "/ctf.xinetd", "w")
-            xinted = xinetd_template.replace("[BINARY]", data["binary"])
+            generate_xinetd(problem, data)
+          generate_docker(problem, data)
 
-            f.write(xinted)
-            f.close()
-
-          if not os.path.exists(problem + "/docker-compose.yml"):
-            if os.path.exists(problem + "/Dockerfile"):
-              f = open(problem + "/docker-compose.yml", "w")
-              compose = compose_template.replace("[PORT]", str(data["port"]))
-            
-              f.write(compose)
-              f.close()
-
-              print("Successfully built " + problem)
-            else:
-              print("Skipping build for " + problem)
         except Exception as exc:
           print("Failed to build " + problem)
 
